14bb6a76c3a87d693bcc98ca780525fa91ef3337
Alvaro Villalobos Vela <vivealed@gmail.com>
2025-09-15 15:05:20 -0400

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import MovieCard from "./Components/MovieCard";
 function App() {
   return (
     <div>
-      <MovieCard />
+      <MovieCard movie={{
+        title: "Movie Name",
+        url: "https://www.youtube.com/watch?v=XXXXXXXXXXX",
+        year: 2023,
+        poster: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+        isFavorite: false
+      }}/>
     </div>
   );
 }
